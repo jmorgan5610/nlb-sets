846f56159276cead89f20204bbbf5c47e4318a8b
Jacob Morgan <jmorgan5610@gmail.com>
2024-09-06 11:06:22 -0400

    Deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/nlb-sets",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
